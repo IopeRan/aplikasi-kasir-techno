efc74d2394f04b8c2434ae01d079562633437b8c
IopeRan <chilliope48@gmail.com>
2023-05-03 06:49:56 +0800

    kapan last commit astaga

--- project/page/dashboard.php
+++ project/page/dashboard.php
@@ -45,7 +45,7 @@ if (!isset($_SESSION["login"])) {
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                         <div class="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
-                                <li class="nav-item active"><a class="nav-link" href="#!"><i class="fa-solid fa-house"></i><span>Home</span></a></li>
+                                <li class="nav-item active"><a class="nav-link" href="dashboard.php"><i class="fa-solid fa-house"></i><span>Home</span></a></li>
                                 <li class="nav-item dropdown">
                                     <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
                                     <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
