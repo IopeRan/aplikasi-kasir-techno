6f7f61ce2a64c1a37822cfa0186f506087ebc4ec
IopeRan <chilliope48@gmail.com>
2023-05-08 11:55:01 +0800

    update apakah

--- project/page/printpdf.php
+++ project/page/printpdf.php
@@ -44,71 +44,7 @@ $getTransaksi = query("SELECT * FROM transaksi");
     </style>
 </head>
 
-<body onload="window.print()">
-    <div class="d-flex" id="wrapper">
-            <!-- Page content-->
-            <div class="container-fluid">
-                <?php
-                if (!in_array("bendahara", $_SESSION['admin_akses'])) {
-                    echo "<div class='alert alert-danger mt-5' role='alert'>
-                        You dont have the permission to acces this page
-                      </div>";
-                    exit();
-                }
-                ?>
-                <div class="table-responsive">
-                    <table class="table table-dark table-striped mt-2 mx-auto" style="width: 100%;">
-                        <thead>
-                            <tr class="text-center">
-                                <th colspan="7">Riwayat Pembelian Techno Gallery</th>
-                            </tr>
-                            <tr class="text-center text-warning">
-                                <!-- <th>ID</th> -->
-                                <th scope="col" width="400">ID</th>
-                                <th width="400" scope="col">Pembeli</th>
-                                <th scope="col" width="400">Tanggal Transaksi</th>
-                                <th scope="col" width="400">Produk</th>
-                                <th scope="col" width="200">Total Produk</th>
-                                <th scope="col">Harga</th>
-                                <th scope="col" width="350">Pembayaran</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php
-                            // $data=$conn->query("SELECT * FROM transaksi");
-                            $no = 1;
-                            $total = 0;
-                            $sold = 0;
-                            // while($getTransaksi = $data->fetch_array()) {
-                            foreach ($getTransaksi as $gt) :
-
-                                $total += $gt["harga"];
-                                $sold += $gt["total"];
-                            ?>
-                                <tr class="text-center">
-                                    <td scope="row"><?= $gt["id"]; ?></td>
-                                    <td><?= $gt["pembeli"]; ?></td>
-                                    <td><?= $gt["tanggal"]; ?></td>
-                                    <td><?= $gt["produk"]; ?></td>
-                                    <td><?= $gt["total"]; ?></td>
-                                    <td><?= $gt["hasil"]; ?></td>
-                                    <td><?= $gt["bayar"]; ?></td>
-                            <?php endforeach; ?>
-                            <tr class="text-center">
-                                <td width="210">Produk Yang Terjual :</td>
-                                <td><?= $sold; ?>&nbsp;&nbsp;unit</td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td width="200">| Total Pendapatan :</td>
-                                <td>Rp.<?= $total; ?></td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
-    </div>
+<body>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="../sweetalert/sweetalert2.all.min.js"></script>
